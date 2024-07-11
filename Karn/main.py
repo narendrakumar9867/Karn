438ccd8cebdae602b2feb7ac2e2aac4a39f9829f
mrnarendra9867 <narendrakumark750@gmail.com>
2024-07-11 16:58:09 +0530

    Day2 work Complete

---
+++ Karn/main.py
@@ -0,0 +1,11 @@
+import os
+
+import eel
+
+#To point www directiry for knowing there is frontend file locate.
+eel.init("www")
+
+#It's a legitimate executable file associated with Microsoft Edge, the default web browser in Windows 10 and later versions. It is responsible for launching and running the Microsoft Edge browser on your system.
+os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+eel.start('index.html' , mode='default', host='localhost', block=True) 
